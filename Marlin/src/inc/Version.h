1ba400c684e47c3428bfb3caf8ae3c4424e2a046
chilman408 <44660877+chilman408@users.noreply.github.com>
2023-04-08 10:51:38 -0700

    Merging upstream changes

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.1.3.1b"
+  #define SHORT_BUILD_VERSION "2.1.3.1b3"
 #endif
 
 /**
